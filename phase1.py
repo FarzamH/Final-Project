4a11c2a268636c66c2f33a9c6eacc2aa477e1a93
FarzamH <farzamhaghighat36@gmail.com>
2024-07-18 12:34:42 -0700

    creating tree with constractor and methods

--- phase1.py
+++ phase1.py
@@ -10,39 +10,6 @@ class ExpressionTree:
     def __init__(self):
         self.root = None
 
-    def evaluate(self, variables):
-        return self._evaluate_node(self.root, variables)
-
-    def _evaluate_node(self, node, variables):
-        if node is None:
-            return 0
-
-        if node.value in variables.keys():
-            return variables[node.value]
-        elif node.value.replace('.', '').isdigit():
-            return float(node.value)
-        elif node.value in ['+', '-', '*', '/']:
-            left = self._evaluate_node(node.left, variables)
-            right = self._evaluate_node(node.right, variables)
-            if node.value == '+':
-                return left + right
-            elif node.value == '-':
-                return left - right
-            elif node.value == '*':
-                return left * right
-            elif node.value == '/':
-                return left / right
-        elif node.value in ['sin', 'cos', 'tan', 'tanh']:
-            arg = self._evaluate_node(node.left, variables)
-            if node.value == 'sin':
-                return math.sin(arg)
-            elif node.value == 'cos':
-                return math.cos(arg)
-            elif node.value == 'tan':
-                return math.tan(arg)
-            elif node.value == 'tanh':
-                return math.tanh(arg)
-
     def create_expression_tree(self, expression):
         tokens = expression.split()
         self.root = self._create_tree(tokens)
@@ -74,6 +41,7 @@ class ExpressionTree:
                 node.left = self._create_tree(tokens[1:])
                 return node
             else:
+                # (sin x) | (x)
                 if tokens[0] == '(' and tokens[-1] == ')':
                     return self._create_tree(tokens[1:-1])
                 else:
@@ -84,22 +52,20 @@ class ExpressionTree:
         node.right = self._create_tree(tokens[operator_index + 1:])
         return node
 
-def evaluate_expressions(expressions, variables):
-    results = []
+
+if __name__ == "__main__":
+    expressions = [
+        "x + 2",
+        "sin x",
+        "x * 3 + 2",
+        "2 + cos x + 1",
+        "2 * x + 3 * x",
+        "sin ( ( x + 1 ) + 1 )"
+    ]
+
+    x = 1
+
     for expr in expressions:
         tree = ExpressionTree()
         tree.create_expression_tree(expr)
-        result = tree.evaluate(variables)
-        results.append(result)
-    return results
-
-expressions = [
-    "x + y",
-    "sin x * cos y",
-    "z * 3 + x * y",
-    "tanh ( x + y ) * z"
-]
-
-variables = {'x': 0.5, 'y': 1.0, 'z': 2.0}
-results = evaluate_expressions(expressions, variables)
-print(f"f{tuple(zip(variables.keys(), variables.values()))} = {tuple(expressions)} = {tuple(results)}")
+        print(f'tree {tree} with expression {expr} created successfully.')
